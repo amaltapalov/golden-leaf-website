1612c496c6d9e360e61cc94a35527ed2fdcb2f11
Amal Tapalov <amaltapalov@gmail.com>
2019-07-12 13:43:12 +0200

    Add slick carousel and overlay layer on images

--- js/master.js
+++ js/master.js
@@ -3,3 +3,14 @@
 (() => {
 	console.log("hello 6");
 })();
+
+// Slick
+$(document).ready(function() {
+	$(".slick-container").slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		// autoplay: true,
+		// autoplaySpeed: 2000,
+		arrows: false
+	});
+});
