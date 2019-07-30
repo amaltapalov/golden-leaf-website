ca964c861b1e7cf621cafc5b855fcc12138082d9
Amal Tapalov <amaltapalov@gmail.com>
2019-07-30 12:28:22 +0200

    Complete Full screen navigation

--- js/fullScreenNav.js
+++ js/fullScreenNav.js
@@ -1,39 +1,19 @@
 // https://codepen.io/RSH87/pen/rmgYbo
 
-const app = (() => {
-	let body;
-	let menu;
-	let menuItems;
+function applyListener() {
+	$("body").toggleClass("nav-active");
 
-	const init = () => {
-		body = document.querySelector("body");
-		menu = document.querySelector(".menu-icon");
-		menuItems = document.querySelectorAll(".nav__list-item");
+	// if ($("body").hasClass("nav-active")) {
+	// 	$(".nav__list-item").fadeIn();
+	// } else {
+	// 	$(".nav__list-item").fadeOut();
+	// }
 
-		applyListeners();
-	};
-
-	const applyListeners = () => {
-		menu.addEventListener("click", () => toggleClass(body, "nav-active"));
-	};
-
-	const toggleClass = (element, stringClass) => {
-		if (element.classList.contains(stringClass))
-			element.classList.remove(stringClass);
-		else element.classList.add(stringClass);
-	};
-
-	init();
-})();
-
-// const body = $("#body-container");
-
-// jQuery("#hamburger").on("click", function() {
-// 	if (body.hasClass("nav-active")) {
-// 		$(".nav__content")
-// 			.delay(1000)
-// 			.fadeIn();
-// 	} else {
-// 		$(".nav__content").fadeOut();
-// 	}
-// });
+	if ($("body").hasClass("nav-active")) {
+		setTimeout(function() {
+			$(".nav__list-item").fadeTo(400, 1);
+		}, 600);
+	} else {
+		$(".nav__list-item").fadeTo(100, 0);
+	}
+}
